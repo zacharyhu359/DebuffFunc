46342fd0bee08474b0b94894ef3b951758d47e45
zacharyhu359 <75653825+zacharyhu359@users.noreply.github.com>
2025-04-06 19:33:45 -0400

    Add files via upload

--- DebuffFunc/DebuffFunc.lua
+++ DebuffFunc/DebuffFunc.lua
@@ -4,7 +4,6 @@ local DBT_Timers ={}
 function DBT_SpellCastTimer(spellName, DotTime )
 	if DBT_Timers[spellName] == nil then DBT_Timers[spellName] = GetTime() end;
 	if SpellReady(spellName) and not buffed(spellName,"target") then DBT_Timers[spellName] = GetTime() end;
-	if IsShiftKeyDown() then DBT_ResetTimers() return false end;
 	if DBT_Timers[spellName] - GetTime() <= 0 and SpellReady(spellName) then CastSpellByName(spellName) DBT_Timers[spellName] = (DotTime + GetTime()) return true end;
 	return false;
 end
@@ -21,13 +20,27 @@ function DBT_ResetTimers()
 	DBT_Timers ={}
 end
 
-function DBT_ReduceDotTime(spellName, timeToReduce)
-	if not DBT_Timers[spellName] == nil then DBT_Timers[spellName] = (DBT_Timers[spellName] - timeToReduce) end;
+function DBT_ResetTimer(spellName)
+		if (not (DBT_Timers[spellName] == nil)) then DBT_Timers[spellName] = GetTime() end;
 end
 
-function DBT_GetTimerTime(spellName)
-	return DBT_Timers[spellName];
+function DBT_ReduceDotTime(spellName, timeToReduce)
+	if (not (DBT_Timers[spellName] == nil)) then DBT_Timers[spellName] = (DBT_Timers[spellName] - timeToReduce) end;
 end
+
+function DBT_SetTimerTime(spellName, DotTime)
+	 DBT_Timers[spellName] = (DotTime + GetTime())
+end
+
+function DBT_GetRemainingTime(spellName)
+    if(not (DBT_Timers[spellName] == nil)) then return (DBT_Timers[spellName] - GetTime()) else return 0 end;
+end
+
+function  DBT_GetElapsedTime(spellName)
+	if (not (DBT_Timers[spellName] == nil)) then return (GetTime() - DBT_Timers[spellName]) end;
+	return -1;
+end
+
 
 
 
